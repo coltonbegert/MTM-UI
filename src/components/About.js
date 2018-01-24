606a2794cb2de2cb47e70ba48b80ede026b6c938
Colton Begert <begert@ualberta.ca>
2018-01-24 13:30:58 -0700

    Added an about page

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,22 @@ export default class About extends React.Component {
   
   render() {
     return (
-      <div></div>
+      <div>
+        <div className= 'container'>
+          <div className='row'>
+            <div className='col-lg-12 text-center'>
+              <h2>About</h2>
+            </div>
+            <div className='col-lg-12 text-center'>
+              <header> Colton Begert </header>
+            </div>
+            <div className='col-lg-12'>
+              <text>Wow look at this beautiful left justified text!</text>
+            </div>
+          </div>
+        </div>
+      </div>
+
     );
   }
   
